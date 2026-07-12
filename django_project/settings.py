a9c2b4282d02259f2d5e9da66803db0978d72b6d
fernandes-draw <fernandes_draw@hotmail.com>
2026-07-11 22:52:25 -0300

    Adiciona domínios da Vercel ao ALLOWED_HOSTS e CSRF

--- django_project/settings.py
+++ django_project/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
+ALLOWED_HOSTS = [".herokuapp.com", ".vercel.app", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com", "https://*.vercel.app"]
 
 
 # Application definition
